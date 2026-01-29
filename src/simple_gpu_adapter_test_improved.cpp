a0b28b624affa596736870c3f7eec2f952e2a208
Hansu Lee <cocobunnyfarm@gmail.com>
2026-01-29 18:57:42 +0900

    temporary

---
+++ src/simple_gpu_adapter_test_improved.cpp
@@ -0,0 +1,91 @@
+/***
+ * Improvements:
+ * 
+ * 1) Use Pinned Memory
+ * cudaHostAlloc(&host_ptr, size, cudaHostAllocDefault);
+ * 
+ */
+
+#include <memory>
+#include <chrono>
+#include <cuda_runtime.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+using namespace std::chrono_literals;
+
+// 1. DATA STRUCTURE
+struct GpuBuffer {
+  float* dev_ptr;
+};
+
+// 2. THE ADAPTER (Must be fully defined before the Node)
+namespace rclcpp {
+template<>
+struct TypeAdapter<GpuBuffer, std_msgs::msg::Float32> {
+  using is_specialized = std::true_type;
+  using custom_type = GpuBuffer;
+  using ros_message_type = std_msgs::msg::Float32;
+
+  static void convert_to_ros_message(const custom_type& source, ros_message_type& dest) {
+    float host_val;
+    cudaMemcpy(&host_val, source.dev_ptr, sizeof(float), cudaMemcpyDeviceToHost);
+    dest.data = host_val;
+  }
+
+  static void convert_to_custom(const ros_message_type& source, custom_type& dest) {
+    cudaMalloc(&dest.dev_ptr, sizeof(float));
+    cudaMemcpy(dest.dev_ptr, &source.data, sizeof(float), cudaMemcpyHostToDevice);
+  }
+};
+
+// This alias is the secret sauce that tells the Publisher GpuBuffer is okay.
+template<>
+struct TypeAdapter<GpuBuffer> : public TypeAdapter<GpuBuffer, std_msgs::msg::Float32> {};
+
+} // namespace rclcpp
+
+// 3. THE NODE
+class GpuBridgeNode : public rclcpp::Node {
+public:
+  GpuBridgeNode() : Node("gpu_bridge_node", 
+    rclcpp::NodeOptions().use_intra_process_comms(true)) 
+  {
+
+
+    // Now the Publisher knows GpuBuffer has a TypeAdapter!
+    pub_ = this->create_publisher<GpuBuffer>("gpu_topic", 10);
+
+    sub_ = this->create_subscription<GpuBuffer>("gpu_topic", 10,
+      [this](const GpuBuffer& msg) {
+        float host_val;
+        cudaMemcpy(&host_val, msg.dev_ptr, sizeof(float), cudaMemcpyDeviceToHost);
+        RCLCPP_INFO(this->get_logger(), "SUB: Got %f from GPU addr %p", host_val, (void*)msg.dev_ptr);
+        cudaFree(msg.dev_ptr);
+      });
+
+    timer_ = this->create_wall_timer(1s, [this]() {
+      auto msg = std::make_unique<GpuBuffer>();
+      cudaMalloc(&msg->dev_ptr, sizeof(float));
+      float val = 3.14159f;
+      cudaMemcpy(msg->dev_ptr, &val, sizeof(float), cudaMemcpyHostToDevice);
+      
+      RCLCPP_INFO(this->get_logger(), "PUB: Sending GPU addr %p", (void*)msg->dev_ptr);
+      pub_->publish(std::move(msg));
+    });
+  }
+
+private:
+  rclcpp::Publisher<GpuBuffer>::SharedPtr pub_;
+  rclcpp::Subscription<GpuBuffer>::SharedPtr sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  
+};
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<GpuBridgeNode>());
+  rclcpp::shutdown();
+  return 0;
+}
